a88e8ab3f1cc9fdef114ed21e8d472b048b3f8d4
Bernabewe <bernybf05@gmail.com>
2023-02-12 12:26:03 -0700

    Vistas PDF iniciadas

--- resources/views/PDF/reporteGenerico.blade.php
+++ resources/views/PDF/reporteGenerico.blade.php
@@ -6,21 +6,35 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Reporte PDF Generico</title>
     <style>
-        h3{
+        /* h3{
             background-color: blue;
             color: white;
             padding: 10px;
             margin-bottom: 15px;
             border: 1px solid black;
             border-radius: 20px;
-        }
+        } */
     </style>
 </head>
 <body>
-    <h1>Reporte Generico</h1>
-    <hr>
-    @foreach ($alumnos as $a)
-        <h3>{{ $a }}</h3>
-    @endforeach
+    <!-- <img src="{{asset('images/encabezado.png')}}" alt=""> -->
+    <div style="text-align: right;">
+        <p>Culiacán Sin. ***Fecha*** <br>
+        OFICIO: ***Fecha oficio***</p>
+    </div>
+    <div style="text-align: center;">
+        <h2>Reporte Generico</h2>
+        <br>
+        <br>
+    </div>
+    <div>
+        <p>Nombre del Alumno(a): NOMBRE Grupo: GRUPO.
+        Especialidad: ESPECIALIDAD Turno: VESPERTINO Motivo MOTIVO.
+        </p>
+        <p>En caso de reincidir en faltas al reglamento, el alumno se hará acreedor a otro tipo
+        de sanción.
+        </p>
+    </div>
+    
 </body>
 </html>
--- resources/views/reporte/consultar.blade.php
+++ resources/views/reporte/consultar.blade.php
@@ -15,22 +15,34 @@
             <thead>
                 <tr>
                     <th>ID</th>
+                    <th>Num. de cotrol</th>
                     <th>Nombre</th>
-                    <th>Edad</th>
+                    <th>Especialida</th>
+                    <th>Tipo de reporte</th>
+                    <th>Orientadora</th>
+                    <th>Fecha de creación</th>
                     <th>Opciones</th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
                     <td>2</td>
+                    <td>12345678910</td>
                     <td>Juan</td>
-                    <td>18</td>
+                    <td>Programación</td>
+                    <td>Indvidual</td>
+                    <td>Maria</td>
+                    <td>12/02/2023</td>
                     <td>
-                        <a href="" class="btn btn-danger btn-sm">
-                            <i class="fas fa-times"></i>
-                        </a>
                         <a href="{{ url('reporte/pdf') }}" class="btn btn-success btn-sm">
                             <i class="far fa-file-pdf"></i>
+                        </a>
+                        <a href="" class="btn btn-primary btn-sm">
+                            <i class="far fa-edit"></i>
+                            <!-- <i class="fas fa-pencil-alt"></i> -->
+                        </a>
+                        <a href="" class="btn btn-danger btn-sm">
+                            <i class="fas fa-times"></i>
                         </a>
                     </td>
                 </tr>

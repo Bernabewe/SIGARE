3b3c981b7d64f9a42e86489dd5d647a9840d1ec8
Sebastian Coronel <sebasproyecto511@gmail.com>
2023-02-13 20:46:48 -0600

    Modificamos Login - Sin Terminar

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,16 +7,16 @@
 
         <!-- Email Address -->
         <div>
-            <x-input-label for="email" :value="__('Email')" />
+            <x-input-label for="email" :value="__('Correo Electronico:')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <x-input-label for="password" :value="__('Contraseña:')" />
 
-            <x-text-input id="password" class="block mt-1 w-full"
+            <x-text-input id="password" class="block mt-1 w-full" 
                             type="password"
                             name="password"
                             required autocomplete="current-password" />
@@ -27,15 +27,15 @@
         <!-- Remember Me -->
         <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                <input id="remember_me" type="checkbox"  class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
+                <span class="ml-2 text-sm text-red-600">{{ __('Recuerdame') }}</span>
             </label>
         </div>
-
+    
         <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
+                    {{ __('Olvidaste tu contraseña?') }}
                 </a>
             @endif
 
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -13,16 +13,17 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
-            </div>
+    <body class="font-sans text-red-900 antialiased">
+        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-1 sm:pt-0 bg-#E5E5E5-100">
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            <div class="w-1000 sm:max-w-md mt-4 px-4 py-2 bg-white shadow-md overflow-hidden sm:rounded-lg">
+                <a href="/">
+                    <img class= "w-100 h-20 fill-current text-red-500" src= "{{asset('/img/Logo.png')}}" />
+                </a><br>
                 {{ $slot }}
+                
+                
+            </div>
             </div>
         </div>
     </body>

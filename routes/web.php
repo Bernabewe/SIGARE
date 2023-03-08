0a40cebfd0d05640cd6ba853be723d0b502e9d30
Sebastian Coronel Aispuro <sebasproyecto511@gmail.com>
2023-03-07 18:46:06 -0700

    Solucion de Errores

--- routes/web.php
+++ routes/web.php
@@ -16,32 +16,16 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+//Route::group(['prefix' => 'orientador', 'middleware' => ['orientador', 'role:orientador']], function(){
+    //Route::get('/home', function(){
+        //return view('orientador.home');
+   // });
+//});
 
-<?php
+//Route::group(['middleware' => ['administrador', 'role:administrador']], function() {
 
-use App\Http\Controllers\ReporteController;
-use App\Http\Controllers\PDFController;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\AlumnoController;
-use Illuminate\Support\Facades\Route;
+    Route::get('/home', [HomeController::class, 'home']);
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-Route::group(['prefix' => 'orientador', 'middleware' => ['orientador', 'role:orientador']], function(){
-    Route::get('/home', function(){
-        return view('orientador.home');
-    });
-});
-
-Route::group(['middleware' => ['administrador', 'role:administrador']], function() {
 
     //Rutas de alumnos
     Route::get('/reporte/consultar', [ReporteController::class, 'consultar']);
@@ -105,7 +89,7 @@ Route::group(['middleware' => ['administrador', 'role:administrador']], function
     Route::get('/reporte/pdfCartaCompromiso', [PDFController::class, 'pdfCartaCompromiso']);
     Route::get('/reporte/pdfCartaBuenaConducta', [PDFController::class, 'pdfCartaBuenaConducta']);
     Route::get('/reporte/pdfCartaCondicional', [PDFController::class, 'pdfCartaCondicional']);
-});
+//});
 
 
 
@@ -113,3 +97,4 @@ Route::get('/blank', function () {
     return view('blankpage');
 });
 
+?>

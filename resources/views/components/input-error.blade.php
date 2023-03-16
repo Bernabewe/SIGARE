33692fa805d1c0581df4307294a0dc74f40deee8
Sebastian Coronel Aispuro <sebasproyecto511@gmail.com>
2023-03-15 21:21:33 -0700

    Login - Modificado

--- resources/views/components/input-error.blade.php
+++ resources/views/components/input-error.blade.php
@@ -1,7 +1,7 @@
 @props(['messages'])
 
 @if ($messages)
-    <ul {{ $attributes->merge(['class' => 'text-sm text-red-600 space-y-1']) }}>
+    <ul style="font-color:#691c32; {{ $attributes->merge(['class' => 'text-sm text-red-600 space-y-1']) }}>
         @foreach ((array) $messages as $message)
             @php
                 $message = 'Ingrese un email o contraseña valido';

c953b2901b39665e77802d721db78e99875f1b9c
Bernabewe <bernybf05@gmail.com>
2023-03-21 20:48:27 -0700

    Problema imagen servidor resuelto

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -137,7 +137,7 @@
           <!-- Brand Logo -->
           <a href="{{asset('/home')}}" class="brand-link">
             <img src="{{ asset('img/Logo.png') }}" alt="baessh" class="brand-image img-circle elevation-3" style="opacity: .8">
-            <span class="brand-text font-weight-light">SIGARE</span>
+            <span class="brand-text font-weight-light"> SIGARE </span>
           </a>
 
           <!-- Sidebar -->

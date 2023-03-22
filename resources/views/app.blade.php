fe946f35713b7f4b73e8157a0d7931d5994a6ab1
Bernabewe <bernybf05@gmail.com>
2023-03-21 20:43:34 -0700

    Servidor imagen arreglada

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -136,7 +136,7 @@
         <aside class="main-sidebar elevation-4 sidebar-light-warning"  style="background-color: #DFDFDF;">
           <!-- Brand Logo -->
           <a href="{{asset('/home')}}" class="brand-link">
-            <img src="{{ asset('img/logo.png') }}" alt="baessh" class="brand-image img-circle elevation-3" style="opacity: .8">
+            <img src="{{ asset('img/Logo.png') }}" alt="baessh" class="brand-image img-circle elevation-3" style="opacity: .8">
             <span class="brand-text font-weight-light">SIGARE</span>
           </a>
 
